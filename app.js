97f9b2574f2367f68ed8b98816428945b231e159
fedegadea <federicosegundogadea@gmail.com>
2022-09-26 23:42:42 -0300

    a

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path= require("path");
-const publicPath= path.resolve(__dirname, "./PUBLIC");
+const publicPath= path.resolve(__dirname, "./public");
 app.use(express.estatic(publicPath));
 
 app.listen(3090,() => { 
@@ -9,6 +9,6 @@ app.listen(3090,() => {
     console.log("Servidor corriendo")
 })
 app.get("/",(req,res)=> {
-    res.sendFile(path.join(__dirname,"/PROG/home.html"))
+    res.sendFile(path.join(__dirname,"/proyectom/home.html"))
 })
 
