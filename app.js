55b286644fb2d4c028174da82fd58cb8ec7ef407
fedegadea <federicosegundogadea@gmail.com>
2022-09-27 10:37:20 -0300

    s

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path= require("path");
 const publicPath= path.resolve(__dirname, "./public");
 app.use(express.estatic(publicPath));
 
-app.listen(3090,() => { 
 
-    console.log("Servidor corriendo")
+console.log("Servidor corriendo")
+app.listen(process.env.PORT || 3090,() => { 
 })
 app.get("/",(req,res)=> {
     res.sendFile(path.join(__dirname,"/proyectom/home.html"))
